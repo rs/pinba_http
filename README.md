28b60d797f3acd8341f3ed92bdc6fc17c724f318
Olivier Poitrey <rs@dailymotion.com>
2011-04-06 03:32:45 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,38 @@
+Pinba HTTP Daemon
+=================
+
+Pinba-HTTPD is a HTTP interface for [Pinba](http://pinba.org/). It allows to forge Pinba timers from simple HTTP queries with optional tags. This is useful to time things on the client (javascript, flash) in the same backend as for you backend code.
+
+This is a bit of a hack as Pinba isn't really meant to do that, but who cares? :)
+
+Usage
+-----
+
+To account the frequency of an event, request the following URL from your client code:
+
+    http://hostname/track/mycounter
+
+This will create a Pinba request, with `script_name` set to `mycounter`. You can then graph the number of request per second for this counter using a query like this:
+
+    SELECT req_per_sec FROM report_by_server_name WHERE script_name = 'mycounter';
+
+You can add a time value to your counter by adding the time in seconds (float) to the request:
+
+    http://hostname/track/mytimer;0.231
+
+The time value will be set as the `request_time` value. You can then get the average time for this timer using a query like this:
+
+    SELECT req_time_total/req_count FROM report_by_server_name WHERE script_name = 'mytimer';
+
+Finaly, you can add tags to your timer by adding a query string to your request. If at least one tag is present, a single sub-timer will be created with the provided tags:
+
+    http://hostname/track/mytimer;0.231?tag1=value1&tag2=value2
+
+You can then create corresponding report table for the tags you want to query.
+
+Installation
+------------
+
+This project requires [Flask](http://flask.pocoo.org/) micro-framework and the [Protocol Buffer Python library](http://code.google.com/apis/protocolbuffers/docs/pythontutorial.html).
+
+Follow the [Flask deployement options](http://flask.pocoo.org/docs/deploying/) corresponding to the WSGI server you want to use. Don't forget to set the `PINBA_HOST` and `PINBA_PORT` configuration variables to your Pinba server.
---
+++ pinba_httpd.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from flask import Flask, request
+from socket import socket, gethostname, AF_INET, SOCK_DGRAM
+from sys import argv
+import pinba_pb2
+
+DEBUG = True
+PINBA_HOST = argv[1] if len(argv) > 1 else '127.0.0.1'
+PINBA_PORT = 30002
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('PINBA_HTTPD_SETTINGS', silent=True)
+
+udpsock = socket(AF_INET, SOCK_DGRAM)
+hostname = gethostname()
+
+@app.route('/track/<tracker>')
+@app.route('/track/<tracker>;<float:timer>')
+def track(tracker, timer=0.0):
+    # Create a default "empty" Pinba request message
+    msg = pinba_pb2.Request()
+    msg.hostname = hostname
+    msg.server_name = request.environ['HTTP_HOST']
+    msg.script_name = tracker
+    msg.request_count = 1
+    msg.document_size = 0
+    msg.memory_peak = 0
+    msg.request_time = timer
+    msg.ru_utime = 0.0
+    msg.ru_stime = 0.0
+    msg.status = 200
+
+    if request.args:
+        # Add a single timer
+        msg.timer_hit_count.append(1)
+        msg.timer_value.append(timer)
+        msg.timer_tag_count.append(len(request.args))
+
+        # Encode associated tags
+        dictionary = [] # contains mapping of tags name or value => uniq id
+        for name, value in request.args.items():
+            value = str(value)
+            if name not in dictionary:
+                dictionary.append(name)
+            if value not in dictionary:
+                dictionary.append(value)
+            msg.timer_tag_name.append(dictionary.index(name))
+            msg.timer_tag_value.append(dictionary.index(value))
+
+        # Global tags dictionary
+        msg.dictionary.extend(dictionary);
+
+    # Send message to Pinba server
+    udpsock.sendto(msg.SerializeToString(), (PINBA_HOST, PINBA_PORT))
+
+    return ''
+
+@app.after_request
+def after_request(response):
+    response.headers['Server'] = 'Pinba-HTTPD/1.0';
+    return response
+
+if __name__ == '__main__':
+    app.run()
